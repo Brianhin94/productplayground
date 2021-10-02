99cbe8064935309b9baf3f421c1c4880010c70a2
victortwu <victor607@gmail.com>
2021-10-02 16:57:06 -0700

    trying to pull

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react'
+import BarcodeReader from 'react-barcode-reader'
 import './App.css';
 
 function App() {
+
+console.log(<BarcodeReader/>)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>get data</button>
+      <BarcodeReader/>
     </div>
   );
 }
 
 export default App;
+// import React, { Component } from 'react'
+// import BarcodeReader from 'react-barcode-reader'
+//
+// class Test extends Component {
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       result: 'No result',
+//     }
+//
+//     this.handleScan = this.handleScan.bind(this)
+//   }
+//   handleScan(data){
+//     this.setState({
+//       result: data,
+//     })
+//   }
+//   handleError(err){
+//     console.error(err)
+//   }
+//   render(){
+//
+//     return(
+//       <div>
+//         <BarcodeReader
+//           onError={this.handleError}
+//           onScan={this.handleScan}
+//           />
+//         <p>{this.state.result}</p>
+//       </div>
+//     )
+//   }
+// }
+// export default Test
