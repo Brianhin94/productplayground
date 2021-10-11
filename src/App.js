88280d671d027970fdcde0fb89025b834383e2c6
Brianhin94 <brianhin64@gmail.com>
2021-10-11 05:51:11 -0400

    going to style these popup elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
 import React from 'react'
-import BarcodeReader from 'react-barcode-reader'
 import './App.css';
 import { useState } from 'react';
 import BarcodeScannerComponent from 'react-qr-barcode-scanner';
-
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 import WebCam from './WebCam'
 
-const axios = require('axios').default;
+
+function App() {
+  const [data, setData] = useState('Not Found');
+  const [buttonPopup, setButtonPopup] = useState('Not Found');
+  return (
+    <div className="App"> 
+    <main>
+        <Popup trigger={<button>1</button>} position="right center">
+    <div>Popup content here !!</div>
+  </Popup>
+  <Popup trigger={<button>2</button>} position="right center">
+    <div>Popup content here !!</div>
+  </Popup>
+  <Popup trigger={<button>3</button>} position="right center">
+    <div>Popup content here !!</div>
+  </Popup>
+  <Popup trigger={<button>4</button>} position="right center">
+    <div>Popup content here !!</div>
+  </Popup>
+  <Popup trigger={<button>5</button>} position="right center">
+    <div>Popup content here !!</div>
+  </Popup>
+  <Popup trigger={<button>6</button>} position="right center">
+    <div>Popup content here !!</div>
+  </Popup>
+  <Popup trigger={<button>7</button>} position="right center">
+    <div>Popup content here !!</div>
+  </Popup>
+
+        <BarcodeScannerComponent
+          width={500}
+          height={500}
+          onUpdate={(err, result) => {
+            if (result) setData(result.text);
+            else setData('Not Found');
+          }}
+        />
+        <p>{data}</p>
+
+        <WebCam/>
+        </main>
+
+    </div>
+  );
+}
+
+
+
+
+
+export default App;
+
+// const axios = require('axios').default;
 
 // axios.get('/user?ID=12345')
 //   .then(function (response) {
@@ -20,44 +72,13 @@ const axios = require('axios').default;
 //   .then(function () {
 //     // always executed
 //   });
-
-function App() {
-  const [data, setData] = useState('Not Found');
-  return (
-    <div className="App">
-
-        <button onClick={() => {
-          fetchRandomData();
-        }}>
-          Activate Lasers
-        </button>
-        <BarcodeScannerComponent
-          width={500}
-          height={500}
-          onUpdate={(err, result) => {
-            if (result) setData(result.text);
-            else setData('Not Found');
-          }}
-        />
-        <p>{data}</p>
-
-        <WebCam/>
-
-    </div>
-  );
-}
-const fetchRandomData = () => {
-  return axios.get('https://randomuser.me/api')
-    .then(res => {
-      //sucess
-      console.log(res);
-    })
-    .catch(err => {
-      console.error(err);
-    });
-};
-
-
-
-
-export default App;
+// const fetchRandomData = () => {
+//   return axios.get('https://randomuser.me/api')
+//     .then(res => {
+//       //sucess
+//       console.log(res);
+//     })
+//     .catch(err => {
+//       console.error(err);
+//     });
+// };
